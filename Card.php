979b0e8471b2b28eeff28da4abc06b327a3c52d1
Rezwanul Hoque <rezwanhoq@hotmail.com>
2019-11-19 23:40:53 +1100

    Refactoring

--- Card.php
+++ Card.php
@@ -1,28 +1,43 @@
 <?php
 
-$cards = array('k', 9, 'q', 'j', 10);
-//$cards = array('k',9,7 ,8, 10);
+$suit = array('c', 'd', 'h', 's');
+$card = array('a', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'j', 'q', 'k');
+$value = array('a' => 1, 'j' => 11, 'q' => 12, 'k' => 13);
+$shuffled = shuffleCards($suit, $card);
+$ss = straightOrStraightFlush($shuffled, $value);
 
-$tst = array('a' => 1, 'j' => 11, 'q' => 12, 'k' => 13);
-$data = array();
+function shuffleCards($suits, $cards) {
 
-foreach ($cards as $card) {
-    if (array_key_exists($card, $tst)) {
-        array_push($data, $tst[$card]);
-    } else {
-        array_push($data, $card);
-    }
-}
-
-rsort($data);
-$last = 0;
-foreach ($data as $value) {
-    if ($last <> 0) {
-        if ((($last - $value) == 1) || (($last == 10) && ($value == 1))) {
-            
-        } else {
-            break;
+    $allCards = array();
+    foreach ($cards as $card) {
+        foreach ($suits as $suit) {
+            array_push($allCards, $card . $suit);
         }
     }
-    $last = $value;
+    shuffle($allCards);
+    $cards = array_slice($allCards, 0, 5, true);
+    return $cards;
 }
+
+function straightOrStraightFlush($shuffledCards, $v) {
+    $data = array();
+    foreach ($shuffledCards as $card) {
+        $card = substr($card, 0, strlen($card) - 1);
+        array_key_exists($card, $v) ? array_push($data, $v[$card]) : array_push($data, $card);
+    }
+    rsort($data);
+    $last = 0;
+    foreach ($data as $value) {
+        if ($last <> 0) {
+            if (($last - $value == 1) || ($last == 10 && $value == 1)) {
+                
+            } else {
+                return false;
+            }
+        }
+        $last = $value;
+    }
+    return true;
+}
+
+?>
