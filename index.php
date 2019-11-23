2c34f09e97d1878b732df984fca611c34927cc0a
Rezwanul Hoque <rezwanhoq@hotmail.com>
2019-11-23 20:19:27 +1100

    index

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+
+include 'Poker.php';
+
+$cc = new poker();
+
+$shuffled = $cc->shuffleCards();
+$output = $cc->straightOrStraightFlush($shuffled);
+var_dump($shuffled);
+var_dump($output);
