15064cdceebbbe29cc1aff3f64162a18ef016eea
Rezwanul Hoque <rezwanhoq@hotmail.com>
2019-11-23 20:32:00 +1100

    More Testing

--- Test/PokerTest.php
+++ Test/PokerTest.php
@@ -23,10 +23,29 @@ class pokerTest extends TestCase {
         $testCards[0] = array('2d', '3d', '4d', '5d', '6d');
         $testCards[1] = array('2d', '3h', '4d', '5h', '6d');
         $testCards[2] = array('aj', 'kj', 'qc', 'jd', '10c');
+        $testCards[3] = array('ac', 'kc', 'qc', 'jc', '10c');
+        $testCards[4] = array('ad', '2d', '3d', '4d', '5d');
+        $testCards[5] = array('ad', '2c', '3h', '4s', '5d');
 
         foreach ($testCards as $value) {
             $result = $cards->straightOrStraightFlush($value);
             $this->assertEquals(True, $result, 'Its not a Straight/StraightFlush');
+        }
+    }
+
+    public function testNotStraight() {
+
+        $cards = new poker();
+        $testCards[0] = array('2d', '10d', '4d', '5d', '6d');
+        $testCards[1] = array('2d', '5h', '8d', '6h', '6d');
+        $testCards[2] = array('2j', '2d', '2c', '2s', '10c');
+        $testCards[3] = array('ac', 'kc', 'qc', 'jc', '9c');
+        $testCards[4] = array('2a', 'ad', 'kc', 'qs', 'js');
+        $testCards[5] = array('ad', '2c', '3h', '4s', 'kd');
+
+        foreach ($testCards as $value) {
+            $result = $cards->straightOrStraightFlush($value);
+            $this->assertEquals(false, $result);
         }
     }
 
